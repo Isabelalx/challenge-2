a8124957543a6cb0a8892e9ba841c2d4315d59e3
Isabelalx <isabelalopesxavier@usf.edu>
2025-01-26 22:03:05 -0500

    Task 1 - Store Inventory

---
+++ cc_2.js
@@ -0,0 +1,6 @@
+// task 1 
+let products = ["blush", "lipstick", "eyeshadow", "foundation", "mascara"];
+products.push("eyeliner");
+console.log(products);
+products.pop();
+console.log("Updated products:",products);  
