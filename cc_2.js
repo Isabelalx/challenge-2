5d0989d9f6c90c2f9cbc2b9e1b7d9905596382b3
Isabelalx <isabelalopesxavier@usf.edu>
2025-01-26 22:11:24 -0500

    Task 2 - Student Scores.

--- cc_2.js
+++ cc_2.js
@@ -4,3 +4,14 @@ products.push("eyeliner");
 console.log(products);
 products.pop();
 console.log("Updated products:",products);  
+
+// task 2
+let scores = [100, 200, 300, 400, 500];
+scores[1] = 150;
+
+let total = scores.reduce((a, b) => a + b, 0);
+let average = total / scores.length;
+
+console.log("Updated scores:", scores);
+console.log("Total scores:", total);
+console.log("Average scores:", average);
